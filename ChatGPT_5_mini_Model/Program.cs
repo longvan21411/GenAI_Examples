6ef7c17481c253889f94a98f8237899b78392c1f
Van Ta Van <tvv2hc@bosch.com>
2026-03-02 15:44:31 +0700

    #dotnetGenAI: add more the Basic completion region comment

--- ChatGPT_5_mini_Model/Program.cs
+++ ChatGPT_5_mini_Model/Program.cs
@@ -24,10 +24,23 @@ var openAIOptions = new OpenAIClientOptions()
     Endpoint = endpoint
 };
 
+#region Basic in completion
 // create a chat client
 var client = new OpenAIClient(credential, openAIOptions).GetChatClient(model).AsIChatClient();
 
 // send prompts to the model
-var response = await client.GetResponseAsync("What is AI? Explain in max 200 worlds");
-Console.WriteLine(response);
+var prompt  = "What is AI? Explain in max 200 words.";
+Console.WriteLine($"user >>> {prompt}");
+Console.WriteLine($"\n");
+
+var response = await client.GetResponseAsync(prompt);
+
+Console.WriteLine($"assistant >>> {response}");
+Console.WriteLine($"\n");
+Console.WriteLine("-----------------------------");
+Console.WriteLine($"Tokens used: {response.Usage?.InputTokenCount}");
+Console.WriteLine($"Tokens out: {response.Usage?.OutputTokenCount}");
+Console.WriteLine($"Total tokens: {response.Usage?.TotalTokenCount} ");
+Console.WriteLine("-----------------------------");
+#endregion
 
